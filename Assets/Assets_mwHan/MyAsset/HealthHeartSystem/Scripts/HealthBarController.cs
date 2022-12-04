41dfaa450e74da018fea06c838361154007b2a79
mwj1205 <mwj1205@naver.com>
2022-12-05 01:47:10 +0900

    add die ui

---
+++ Assets/Assets_mwHan/MyAsset/HealthHeartSystem/Scripts/HealthBarController.cs
@@ -0,0 +1,79 @@
+﻿/*
+ *  Author: ariel oliveira [o.arielg@gmail.com]
+ */
+
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBarController : MonoBehaviour
+{
+    private GameObject[] heartContainers;
+    private Image[] heartFills;
+
+    public Transform heartsParent;
+    public GameObject heartContainerPrefab;
+
+    private void Start()
+    {
+        // Should I use lists? Maybe :)
+        heartContainers = new GameObject[(int)PlayerController.Instance.MaxHealth];
+        heartFills = new Image[(int)PlayerController.Instance.MaxHealth];
+
+        PlayerController.Instance.onHealthChangedCallback += UpdateHeartsHUD;
+        InstantiateHeartContainers();
+        UpdateHeartsHUD();
+    }
+
+    public void UpdateHeartsHUD()
+    {
+        SetHeartContainers();
+        SetFilledHearts();
+    }
+
+    void SetHeartContainers()
+    {
+        for (int i = 0; i < heartContainers.Length; i++)
+        {
+            if (i < PlayerController.Instance.MaxHealth)
+            {
+                heartContainers[i].SetActive(true);
+            }
+            else
+            {
+                heartContainers[i].SetActive(false);
+            }
+        }
+    }
+
+    void SetFilledHearts()
+    {
+        for (int i = 0; i < heartFills.Length; i++)
+        {
+            if (i < PlayerController.Instance.Health)
+            {
+                heartFills[i].fillAmount = 1;
+            }
+            else
+            {
+                heartFills[i].fillAmount = 0;
+            }
+        }
+
+        if (PlayerController.Instance.Health % 1 != 0)
+        {
+            int lastPos = Mathf.FloorToInt(PlayerController.Instance.Health);
+            heartFills[lastPos].fillAmount = PlayerController.Instance.Health % 1;
+        }
+    }
+
+    void InstantiateHeartContainers()
+    {
+        for (int i = 0; i < PlayerController.Instance.MaxHealth; i++)
+        {
+            GameObject temp = Instantiate(heartContainerPrefab);
+            temp.transform.SetParent(heartsParent, false);
+            heartContainers[i] = temp;
+            heartFills[i] = temp.transform.Find("HeartFill").GetComponent<Image>();
+        }
+    }
+}
---
+++ Assets/Assets_mwHan/MyAsset/HealthHeartSystem/Scripts/HealthBarHUDTester.cs
@@ -0,0 +1,18 @@
+﻿/*
+ *  Author: ariel oliveira [o.arielg@gmail.com]
+ */
+
+using UnityEngine;
+
+public class HealthBarHUDTester : MonoBehaviour
+{
+    public void Heal(float health)
+    {
+        PlayerController.Instance.Heal(health);
+    }
+
+    public void Hurt(float dmg)
+    {
+        PlayerController.Instance.TakeDamage(dmg);
+    }
+}
