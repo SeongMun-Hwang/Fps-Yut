3e5bd6f8ef483a6b0dd12c62c0fcd2ba71eb3f3b
Minu Han <mwj1205@naver.com>
2023-09-16 10:46:07 +0900

    구조변경
    player를 object로 포괄적으로 변경

--- Assets/Assets_mwHan/Scripts/Managers/Contents/ObjectManager.cs
+++ Assets/Assets_mwHan/Scripts/Managers/Contents/ObjectManager.cs
@@ -9,13 +9,13 @@ public class ObjectManager
 	public MyPlayerController MyPlayer { get; set; }
 	Dictionary<int, GameObject> _objects = new Dictionary<int, GameObject>();
 	
-	public void Add(PlayerInfo info, bool myPlayer = false)
+	public void Add(ObjectInfo info, bool myPlayer = false)
 	{
 		if (myPlayer)
 		{
 			GameObject go = Managers.Resource.Instantiate("Player/MyPlayer");
 			go.name = info.Name;
-			_objects.Add(info.PlayerId, go);
+			_objects.Add(info.ObjectId, go);
 
 			MyPlayer = go.GetComponent<MyPlayerController>();
 			MyPlayer.PosInfo = info.PosInfo;
@@ -26,10 +26,10 @@ public class ObjectManager
 		{
 			GameObject go = Managers.Resource.Instantiate("Player/OtherPlayer");
 			go.name = info.Name;
-			_objects.Add(info.PlayerId, go);
+			_objects.Add(info.ObjectId, go);
 
 			PlayerController pc = go.GetComponent<PlayerController>();
-			pc.Id = info.PlayerId;
+			pc.Id = info.ObjectId;
 			pc.PosInfo = info.PosInfo;
 			pc.RotInfo = info.RotInfo;
 			pc.Stat = info.StatInfo;
