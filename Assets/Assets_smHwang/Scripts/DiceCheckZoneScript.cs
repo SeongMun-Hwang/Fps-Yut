7f18509b5ff624615d3828759eb734b90c17f51b
seongmun-hwang <hwang105814@gmail.com>
2023-10-14 01:55:00 +0900

    윷 만듬

---
+++ Assets/Assets_smHwang/Scripts/DiceCheckZoneScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DiceCheckZoneScript : MonoBehaviour {
+
+	Vector3 diceVelocity;
+
+	// Update is called once per frame
+	void FixedUpdate () {
+		diceVelocity = DiceScript.diceVelocity;
+	}
+
+	void OnTriggerStay(Collider col)
+	{
+		if (diceVelocity.x == 0f && diceVelocity.y == 0f && diceVelocity.z == 0f)
+		{
+			switch (col.gameObject.name) {
+			case "top":
+				DiceNumberTextScript.diceNumber = 1;
+				break;
+			case "bottom":
+				DiceNumberTextScript.diceNumber = 2;
+				break;
+			}
+		}
+	}
+}
---
+++ Assets/Assets_smHwang/Scripts/DiceNumberTextScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class DiceNumberTextScript : MonoBehaviour {
+
+	TextMeshProUGUI text;
+	public static int diceNumber;
+
+	// Use this for initialization
+	void Start () {
+		text = GetComponent<TextMeshProUGUI> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (diceNumber == 1)
+        {
+			text.text = "아래";
+		}
+		else if (diceNumber == 2)
+		{
+			text.text = "위";
+		}
+	}
+}
