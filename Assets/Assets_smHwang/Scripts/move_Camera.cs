3aba47e9720dbd77ad25f26fcc0c5b483cf21e3c
SeongMun-Hwang <hwang105814@gmail.com>
2023-10-17 12:02:22 +0900

    카메라 이동조건 추가

--- Assets/Assets_smHwang/Scripts/move_Camera.cs
+++ Assets/Assets_smHwang/Scripts/move_Camera.cs
@@ -57,21 +57,24 @@ public class move_Camera : MonoBehaviour
     {
         //returns the basic values, if it's 0 then it's not active.
         Vector3 p_Velocity = new Vector3();
-        if (Input.GetKey(KeyCode.W))
+        if (!stone.isFight)
         {
-            p_Velocity += new Vector3(0, 0, 1);
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            p_Velocity += new Vector3(0, 0, -1);
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            p_Velocity += new Vector3(-1, 0, 0);
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            p_Velocity += new Vector3(1, 0, 0);
+            if (Input.GetKey(KeyCode.W))
+            {
+                p_Velocity += new Vector3(0, 0, 1);
+            }
+            if (Input.GetKey(KeyCode.S))
+            {
+                p_Velocity += new Vector3(0, 0, -1);
+            }
+            if (Input.GetKey(KeyCode.A))
+            {
+                p_Velocity += new Vector3(-1, 0, 0);
+            }
+            if (Input.GetKey(KeyCode.D))
+            {
+                p_Velocity += new Vector3(1, 0, 0);
+            }
         }
         return p_Velocity;
     }
