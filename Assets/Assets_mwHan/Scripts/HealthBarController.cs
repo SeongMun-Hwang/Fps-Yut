2651cb6b6493c59b29c270663a04fd980d67c25e
hwang105814 <hwang105814@gmail.com>
2024-07-10 20:58:56 +0900

    Add player & enemy to hammer game

---
+++ Assets/Assets_mwHan/Scripts/HealthBarController.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBarController : MonoBehaviour
+{
+    private GameObject[] heartContainers;
+    private Image[] heartFills;
+
+    public Transform heartsParent;
+    public GameObject heartContainerPrefab;
+    public GameObject player;
+
+    private void Start()
+    {
+
+        player.GetComponent<PlayerController>().onHealthChangedCallback += UpdateHeartsHUD;
+        InstantiateHeartContainers();
+        UpdateHeartsHUD();
+    }
+
+    public void UpdateHeartsHUD()
+    {
+        SetHeartContainers();
+        SetFilledHearts();
+    }
+
+    void SetHeartContainers()
+    {
+
+    }
+
+    void SetFilledHearts()
+    {
+
+
+
+    }
+
+    void InstantiateHeartContainers()
+    {
+
+    }
+}
--- Assets/Assets_mwHan/Scripts/WeaponController.cs
+++ Assets/Assets_mwHan/Scripts/WeaponController.cs
@@ -4,53 +4,56 @@ using UnityEngine;
 
 public class WeaponController : MonoBehaviour
 {
-    // PlayerController 스크립트 참조
-    public PlayerController playerController;
-
-    public GameObject owner;
-    public GameObject Owner 
-    { 
-        get { return owner; }
-        set { owner = value; }
-    }
-
     public GameObject Hammer;
+    public bool canAttack = true;
     public float AttackCooldown = 1f;
-
-
+    //public AudioClip HammerAttackSound;
     public bool IsAttacking = false;
     public BoxCollider meleeArea;
     public TrailRenderer trailEffect;
 
     public float KnockbackForce = 15f;
-    public float KnockbackForceY = 5f;
+    public float KnockbackForceY = 3f;
     public float KnockbackTime = 2f;
 
+    // PlayerController 스크립트 참조
+    public PlayerController playerController;
+
     // 플레이어 맞은 후 움직일 수 없도록 하는 변수, 타이머
+    private bool isPlayerHit = false;
+    private float playerHitTimer = 0f;
     public float playerHitDuration = 3f;
 
-    protected void Start()
+    void Update()
     {
-        playerController = GetComponentInParent<PlayerController>();
-        Owner = playerController.gameObject;
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (canAttack)
+            {
+                HammerAttack();
+            }
+        }
     }
 
-    public void Attack()
+    public void HammerAttack()
     {
-        playerController.canattack = false;
+        canAttack = false;
         Animator anim = Hammer.GetComponent<Animator>();
+        //AudioSource ac = GetComponent<AudioSource>();
         anim.SetTrigger("Attack");
+        //ac.PlayOneShot(HammerAttackSound);
         StartCoroutine(ResetAttackCooldown());
+
     }
 
-    protected IEnumerator ResetAttackCooldown()
+    IEnumerator ResetAttackCooldown()
     {
         StartCoroutine(ResetAttackBool());
         yield return new WaitForSeconds(AttackCooldown);
-        playerController.canattack = true;
+        canAttack = true;
     }
 
-    protected IEnumerator ResetAttackBool()
+    IEnumerator ResetAttackBool()
     {
         yield return new WaitForSeconds(0.1f);
         trailEffect.enabled = true;
@@ -63,5 +66,12 @@ public class WeaponController : MonoBehaviour
 
         yield return new WaitForSeconds(0.25f);
         trailEffect.enabled = false;
+    }
+
+    public void OnPlayerHit()
+    {
+        isPlayerHit = true;
+        playerHitTimer = playerHitDuration;
+        playerController.isKnockedBack = true;
     }
 }
